82e487e38b27eeb6e18f24b1d467d99b16f29f74
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-12-03 15:05:13 -0800

    completed client to connect to nytserver

--- src/App.js
+++ src/App.js
@@ -1,11 +1,104 @@
 import React from 'react';
+import './App.css';
+import Book from './book/book';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			books: [],
+			search: '',
+			sort: '',
+			error: null
+		}
+	}
+
+	//methods to update the state
+	setSearch(search){
+		this.setState({
+			search
+		})
+	}
+
+	setSort(sort){
+		this.setState({
+			sort
+		})
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+
+		//construct a URL with the query string
+		const baseUrl = 'http://localhost:8000/books';
+		const params = [];
+		if (this.state.search) {
+			params.push(`search=${this.state.search}`);
+		}
+		if (this.state.sort){
+			params.push(`sort=${this.state.sort}`);
+		}
+
+		const query = params.join('&');
+		const url = `${baseUrl}?${query}`;
+
+		fetch(url)
+			.then(res => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then(data => {
+				this.setState({
+					books: data,
+					error: null //reset errors
+				})
+			})
+			.catch(err => {
+				this.setState({
+					error: 'Sorry, could not get books at this time'
+				})
+			})
+	}
+
+	render(){
+		//display list of books and the form with onChange listeners to the
+		//components for updating the state as the user selects values
+		const books = this.state.books.map((book, i) => {
+			return <Book {...book} key={i} />
+		})
+
+		return (
+			<main className='App'>
+				<h1>NYT Best Sellers</h1>
+
+				<div className="search">
+					<form onSubmit={ e => this.handleSubmit(e)}>
+						<label htmlFor="search">Search: </label>
+						<input
+							type="text"
+							id="search"
+							name="search"
+							value={this.state.search}
+							onChange={ e => this.setSearch(e.target.value)} />
+						
+						<label htmlFor="sort">Sort:</label>
+						<select id="sort" name="sort" onChange={ e => this.setSort(e.target.value)}>
+							<option value="">None</option>
+							<option value="title">Title</option>
+							<option value="rank">Rank</option>
+						</select>
+
+						<button type="submit">Search</button>
+					</form>
+					<div className="App_error">{ this.state.error }</div>
+				</div>
+				{ books }
+			</main>
+		);
+
+	}
 }
 
 export default App;
